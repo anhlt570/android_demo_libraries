373578b15914f2949b81dafd4151a7443511e311
tuananh <anh.le.tuan@ntq-solution.com.vn>
2018-04-18 15:59:08 +0700

    + Create Ui components demo - Add some Espresso test to main project

--- app/src/main/java/demo/com/demolibraries/MainActivity.java
+++ app/src/main/java/demo/com/demolibraries/MainActivity.java
@@ -1,14 +1,33 @@
 package demo.com.demolibraries;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import demo.com.ui_components.UIActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_open_ui_components).setOnClickListener(this);
     }
+
+    public void openActivity(Class<?> activity) {
+        startActivity(new Intent(this, activity));
+    }
+
+    //View.OnClickListener_
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_open_ui_components:
+                openActivity(UIActivity.class);
+                break;
+        }
+    }
+    //_View.OnClickListener
 }
---
+++ ui_components/src/main/java/demo/com/ui_components/UIActivity.java
@@ -0,0 +1,12 @@
+package demo.com.ui_components;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class UIActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
