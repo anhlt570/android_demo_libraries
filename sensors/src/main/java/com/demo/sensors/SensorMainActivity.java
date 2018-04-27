cb6be0ba6fabfb845921101583c02568f886e965
tuananh <anh.le.tuan@ntq-solution.com.vn>
2018-04-27 17:54:26 +0700

    - Implement Accelerometer and temperature sensors

--- sensors/src/main/java/com/demo/sensors/SensorMainActivity.java
+++ sensors/src/main/java/com/demo/sensors/SensorMainActivity.java
@@ -1,9 +1,6 @@
 package com.demo.sensors;
 
 import android.arch.lifecycle.ViewModelProvider;
-import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
