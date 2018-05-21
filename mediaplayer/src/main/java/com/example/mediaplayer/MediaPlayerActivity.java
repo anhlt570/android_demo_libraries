4a6184e1be5a56d0c6922ad4fbe8e5d98cbee79e
tuananh <anh.le.tuan@ntq-solution.com.vn>
2018-05-21 14:30:43 +0700

    - Play some music - Fix weird error related to android.arch.lifecycle:extensions

---
+++ mediaplayer/src/main/java/com/example/mediaplayer/MediaPlayerActivity.java
@@ -0,0 +1,85 @@
+package com.example.mediaplayer;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.net.MulticastSocket;
+
+public class MediaPlayerActivity extends AppCompatActivity implements View.OnClickListener {
+    public AssetFileDescriptor hellManHigh, hellManLow;
+    private MediaPlayer mMusicPlayer;
+    private AudioManager mAudioManager;
+    Button btnPlay, btnStop;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_player);
+        btnPlay = findViewById(R.id.btn_play);
+        btnStop = findViewById(R.id.btn_stop);
+
+        btnPlay.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+        initMedia();
+    }
+
+    public void initMedia() {
+        try {
+            hellManHigh = getAssets().openFd("Hell Man [high].m4a");
+            hellManLow = getAssets().openFd("Hell Man [low].mp3");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        assert mAudioManager != null;
+        mAudioManager.setMode(AudioManager.MODE_NORMAL);
+        mMusicPlayer = new MediaPlayer();
+        try {
+
+            mMusicPlayer.setDataSource(hellManHigh.getFileDescriptor(), hellManHigh.getStartOffset(), hellManHigh.getLength());
+            mMusicPlayer.prepare();
+            mMusicPlayer.setLooping(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() {
+        mMusicPlayer.start();
+    }
+
+    public void pause() {
+        mMusicPlayer.pause();
+    }
+
+    public void stop() {
+        mMusicPlayer.stop();
+    }
+
+    //View.OnClickListener_
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_play) {
+            if (mMusicPlayer.isPlaying()) {
+                btnPlay.setBackgroundResource(R.drawable.pause);
+                pause();
+            } else {
+                btnPlay.setBackgroundResource(R.drawable.play);
+                play();
+            }
+        } else if (id == R.id.btn_stop) {
+            stop();
+        }
+    }
+    //_View.OnClickListener
+}
