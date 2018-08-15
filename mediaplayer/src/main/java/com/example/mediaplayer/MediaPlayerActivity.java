b3717e8212b04a01239543775d82f1f7479925b8
tuananh <anh.le.tuan@ntq-solution.com.vn>
2018-08-15 16:33:56 +0700

    - Modify Media player module

--- mediaplayer/src/main/java/com/example/mediaplayer/MediaPlayerActivity.java
+++ mediaplayer/src/main/java/com/example/mediaplayer/MediaPlayerActivity.java
@@ -1,49 +1,38 @@
 package com.example.mediaplayer;
 
 import android.content.Context;
-import android.content.res.AssetFileDescriptor;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import java.io.IOException;
-import java.net.MulticastSocket;
 
-public class MediaPlayerActivity extends AppCompatActivity implements View.OnClickListener {
-    public AssetFileDescriptor hellManHigh, hellManLow;
+public class MediaPlayerActivity extends AppCompatActivity {
     private MediaPlayer mMusicPlayer;
     private AudioManager mAudioManager;
-    Button btnPlay, btnStop;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_player);
-
-        btnPlay.setOnClickListener(this);
-        btnStop.setOnClickListener(this);
+        ClickListener clickListener = new ClickListener(this);
+        findViewById(R.id.btn_video).setOnClickListener(clickListener);
+        findViewById(R.id.btn_audio).setOnClickListener(clickListener);
         initMedia();
     }
 
     public void initMedia() {
-        try {
-            hellManHigh = getAssets().openFd("Hell Man [high].m4a");
-            hellManLow = getAssets().openFd("Hell Man [low].mp3");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         assert mAudioManager != null;
         mAudioManager.setMode(AudioManager.MODE_NORMAL);
         mMusicPlayer = new MediaPlayer();
         try {
-
-            mMusicPlayer.setDataSource(hellManHigh.getFileDescriptor(), hellManHigh.getStartOffset(), hellManHigh.getLength());
+            mMusicPlayer.setDataSource(this, Uri.parse("https://zmp3-mp3-s1-te-vnso-qt-3.zadn.vn/57a44002914678182157/7136304650540504646?authen=exp=1534401383~acl=/57a44002914678182157/*~hmac=f454eb92d992577b1f7f542378edc097"));
             mMusicPlayer.prepare();
             mMusicPlayer.setLooping(true);
         } catch (IOException e) {
@@ -63,9 +52,31 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
         mMusicPlayer.stop();
     }
 
-    //View.OnClickListener_
-    @Override
-    public void onClick(View v) {
+    boolean isPlayingMusic() {
+        return mMusicPlayer.isPlaying();
     }
-    //_View.OnClickListener
+
+    public void changePlayMusicState() {
+        if (isPlayingMusic()) {
+            mMusicPlayer.pause();
+        } else {
+            play();
+        }
+    }
+
+    public void playVideo(){
+        Uri resource = Uri.parse("https://zmp3-mv-mcloud-bf-s7-te-vnso-qt-3.zadn.vn/jxlJuU0gY_s/1c7b56f5d0b039ee60a1/058b7e344971a02ff960/480/Duyen-Minh-Lo.mp4?authen=exp=1534477721~acl=/jxlJuU0gY_s/*~hmac=38cd5e4731ba2cf4d5de24bbae77e6d0");
+        final VideoView videoView = findViewById(R.id.videoView);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(resource);
+//        videoView.setZOrderOnTop(true);
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoView.start();
+            }
+        });
+    }
 }
