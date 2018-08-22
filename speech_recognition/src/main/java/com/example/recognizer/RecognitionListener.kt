1f87cf6d274cae50c603a6ade5be1536b009716f
tuananh <anh.le.tuan@ntq-solution.com.vn>
2018-08-22 18:39:03 +0700

    - Add view to show recording animation

--- speech_recognition/src/main/java/com/example/recognizer/RecognitionListener.kt
+++ speech_recognition/src/main/java/com/example/recognizer/RecognitionListener.kt
@@ -2,28 +2,30 @@ package com.example.recognizer
 
 import android.os.Bundle
 import android.speech.RecognitionListener
+import android.speech.SpeechRecognizer
 import android.util.Log
 
-class RecognitionListener(speechRecognitionActivity: SpeechRecognitionActivity) : RecognitionListener {
+class RecognitionListener(speechRecognitionActivity: SpeechRecognitionActivity?) : RecognitionListener {
+
     val TAG = "RecognitionListener"
     override fun onReadyForSpeech(params: Bundle?) {
-        Log.d(TAG, "onReadyForSpeech: " + params )
+        Log.d(TAG, "onReadyForSpeech: $params")
     }
 
     override fun onRmsChanged(rmsdB: Float) {
-        Log.d(TAG, "onRmsChanged: " + rmsdB )
+//        Log.d(TAG, "onRmsChanged: $rmsdB")
     }
 
     override fun onBufferReceived(buffer: ByteArray?) {
-        Log.d(TAG, "onBufferReceived: " + buffer )
+        Log.d(TAG, "onBufferReceived: $buffer")
     }
 
     override fun onPartialResults(partialResults: Bundle?) {
-        Log.d(TAG, "onPartialResults: " + partialResults )
+        Log.d(TAG, "onPartialResults: $partialResults")
     }
 
     override fun onEvent(eventType: Int, params: Bundle?) {
-        Log.d(TAG, "onEvent: " + eventType + " - "+ params )
+        Log.d(TAG, "onEvent: $eventType - $params")
     }
 
     override fun onBeginningOfSpeech() {
@@ -35,10 +37,24 @@ class RecognitionListener(speechRecognitionActivity: SpeechRecognitionActivity) 
     }
 
     override fun onError(error: Int) {
-        Log.d(TAG, "onError: " + error )
+        Log.d(TAG, "onError: $error")
     }
 
     override fun onResults(results: Bundle?) {
-        Log.d(TAG, "onResults: " + results )
+        Log.d(TAG, "OnResults: ")
+        var resultStr = ""
+        results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.let {
+            for (s: String in it) {
+                resultStr += "$s/"
+            }
+        }
+        Log.d(TAG, resultStr)
+        var confidentResult = ""
+        results?.getFloatArray(SpeechRecognizer.CONFIDENCE_SCORES)?.let {
+            for (i: Float in it) {
+                confidentResult += "$i/"
+            }
+        }
+        Log.d(TAG, confidentResult)
     }
 }
